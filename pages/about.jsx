e01c5d83277595f65dff4cf473239a7e1c9b830e
juramirezs <juramirezs@canvia.com>
2024-02-10 17:39:36 -0500

    cambios

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+export default function AboutPage() {
+    return (
+        <Layout title="About DJ Events">
+            <h1>About Page</h1>
+            <p>Main source of all the events featuring DJs</p>
+            <p>Version 1.0</p>
+        </Layout>
+    );
+}
